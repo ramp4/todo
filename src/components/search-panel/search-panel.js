939f196eaa4f733b777f0cba090a707b9b2c3f19
ramp4 <rampatv13@gmail.com>
2020-06-21 10:58:24 +0300

    finish todo app

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import './search-panel.css';
+import "./search-panel.css";
 
-const SearchPanel = () => {
-  return (
-    <input type="text"
-              className="form-control search-input"
-              placeholder="type to search" />
-  );
-};
+export default class SearchPanel extends Component {
+  state = {
+    term: "",
+  };
 
-export default SearchPanel;
+  onTermChange = (e) => {
+    const { onSearchChange = () => {} } = this.props;
+    this.setState({
+      term: e.target.value,
+    });
+
+    onSearchChange(e.target.value);
+  };
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="Type to search"
+        value={this.state.term}
+        onChange={this.onTermChange}
+      />
+    );
+  }
+}
